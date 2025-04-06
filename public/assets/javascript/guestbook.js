d61a5ac8572ea13c31e46cfe2ccca31f48660806
Bechno Kid <bechnokid@gmail.com>
2025-04-06 18:02:09 -0400

    Update for 05 Apr 2025

---
+++ public/assets/javascript/guestbook.js
@@ -0,0 +1,73 @@
+$(function(){
+  let form = $("#guestbooks___guestbook-form");
+
+  form.on('submit', async function(e){
+    e.preventDefault();
+
+    let formData = new FormData(this);
+    const response = await fetch('https://guestbooks.meadow.cafe/guestbook/484/submit', {
+      method: 'POST',
+      body: formData
+    })
+
+    var errorMsgBox = $(".guestbook-error-msg");
+    if (response.ok) {
+      this.reset();
+      loadGuestbookMsgs();
+      errorMsgBox.toggleClass('d-none');
+      errorMsgBox.html('');
+    } else {
+      errorMsgBox.toggleClass('d-none');
+      let errorMsg = (response.status === 401) ? 'The answer is incorrect. Please try again.' : await response.text();
+      errorMsgBox.html(`<p>${errorMsg}</p>`);
+    }
+  })
+
+  function loadGuestbookMsgs() {
+    $.ajax({
+      method: 'GET',
+      url: 'https://guestbooks.meadow.cafe/api/v1/get-guestbook-messages/484',
+      dataType: 'json',
+      success: function(response) {
+        let msgDivArr = []
+        for (let idx = 0; idx < response.length; idx++) {
+          let message = response[idx];
+          let dateObj = new Date(message.CreatedAt);
+          let formattedDate = dateObj.toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+          });
+          let msgArr = [
+            `<div class='guestbook-msg-div'>`,
+              `<div class="guestbook-msg-header">`,
+                `<p><b>Name: </b>${message.Name}</p>`,
+                `<p><b>Sent: </b>${formattedDate}</p>`,
+              `</div>`,
+              `<div class="guestbook-msg-body">`,
+                `<p>${message.Text}</p>`,
+              `</div>`,
+            `</div>`
+          ];
+
+          if (message.Website){
+            msgArr.splice(3, 0, `<p><b>Website: </b><a href='${message.Website}'>${message.Website}</a></p>`)
+          }
+          if (idx > 0) {
+            msgArr.splice(msgArr.length - 1, 0, "<hr class='small'>");
+          }
+
+          msgDivArr.unshift(msgArr.join(''))
+        }
+        let content = (msgDivArr.length > 0) ? msgDivArr.join('') : `<p>There are no messages, yet.</p>`
+        $(".guestbook-msg-container .content").html(content);
+      },
+      error: function(){
+        let errorMsg = 'Unable to obtain guestbook messages.'
+        $(".guestbook-msg-container .content").html(`<p>${errorMsg}</p>`);
+      }
+    });
+  }
+
+  loadGuestbookMsgs();
+})
